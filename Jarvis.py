2943299ba1795241faecea5cd0605e69daa48d12
Presiraji <143067071+Presiraji@users.noreply.github.com>
2024-05-25 01:03:41 -0400

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -7,7 +7,7 @@ st.title("gpt-3.5-turbo-16k-0613")
 client = OpenAI(api_key="sk-balraj-KLoW4HxnPDr6efjrLIFlT3BlbkFJFey4fhZcJMWgg1zIqmyB")
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo-16k-0613"
+    st.session_state["openai_model"] = "gpt-4o"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
